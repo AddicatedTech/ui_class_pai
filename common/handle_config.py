e506b9e597468ee24074a977c0a06070c8aa2b39
AddicatedTech <58582958+AddicatedTech@users.noreply.github.com>
2020-10-15 16:55:08 +0900

    Update handle_config.py

--- common/handle_config.py
+++ common/handle_config.py
@@ -1,11 +1,4 @@
-"""
-============================
-Author:柠檬班-木森
-Time:2020/3/17   21:42
-E-mail:3247119728@qq.com
-Company:湖南零檬信息技术有限公司
-============================
-"""
+
 
 
 from configparser import ConfigParser
